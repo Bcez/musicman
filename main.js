ac3ce5cca0ceea319d64a834d951d55a2e808a67
Bcez <80232341+Bcez@users.noreply.github.com>
2022-10-10 19:32:35 -0400

    Part 2 Test

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
 
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+// Sound System //
+
+song = "";
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+function play(){
+    song.play();
+}
+
+// Drawing System //
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
